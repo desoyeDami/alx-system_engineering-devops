8a79bb76576573e7b38b0f9c2d3b5daa4b1d8464
desoyedami <desoyedami@gmail.com>
2024-07-05 17:36:30 -0700

    Correct

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,33 +1,21 @@
-# Puppet manifest to configure SSH client settings
-# Ensures SSH client uses specified private key and disables password authentication
-file { '/root/.ssh':
+# Puppet manifest to configure global SSH client settings
+
+file { '/etc/ssh':
   ensure  => 'directory',
   owner   => 'root',
   group   => 'root',
-  mode    => '0700',
+  mode    => '0755',
 }
 
-file { '/root/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure  => 'file',
   owner   => 'root',
   group   => 'root',
-  mode    => '0600',
+  mode    => '0644',
   content => @("EOF"),
-Host my-server
+# Global SSH client configuration
+Host *
     IdentityFile ~/.ssh/school
     PasswordAuthentication no
 EOF
 }
-
-file_line { 'Turn off passwd auth':
-  path  => '/root/.ssh/config',
-  line  => '    PasswordAuthentication no',
-  match => '^    PasswordAuthentication',
-}
-
-file_line { 'Declare identity file':
-  path  => '/root/.ssh/config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => '^    IdentityFile',
-}
-
