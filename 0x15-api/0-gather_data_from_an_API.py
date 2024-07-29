5650c206faa8b0d599c8576b9bab4a7fda1ff4a1
desoyedami <desoyedami@gmail.com>
2024-07-29 15:09:34 -0700

    Gather data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+    GENERAL_URL = "https://jsonplaceholder.typicode.com/"
+
+    # Employee Deta
+    EMPLOYEE_URL = f"{GENERAL_URL}/users/{employee_id}"
+    EMPLOYEE_RESPONSE = requests.get(EMPLOYEE_URL).json()
+    EMPLOYEE_NAME = EMPLOYEE_RESPONSE.get('name')
+
+    # Employee Todo list
+    TODO_URL = f"{GENERAL_URL}/todos?userId={employee_id}"
+    TODOs = requests.get(TODO_URL).json()
+
+    # Completed task
+    TASKS_COMPLETED = [todo for todo in TODOs if todo.get('completed')]
+    TOTAL_NUMBER_OF_TASKS = len(TODOs)
+    NUMBER_OF_DONE_TASKS = len(TASKS_COMPLETED)
+
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks"
+          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for TASK in TASKS_COMPLETED:
+        print(f"\t {TASK.get('title')}")
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
