a4b72dbfa2e708193a34a767438f98f976821796
desoyeDami <desoyedami@gmail.com>
2024-08-15 00:47:10 +0100

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
