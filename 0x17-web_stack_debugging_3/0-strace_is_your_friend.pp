7b24a9833494d0921f2f1d3d57de6103a590dd11
desoyeDami <desoyedami@gmail.com>
2024-08-14 23:16:56 +0100

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,29 @@
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Ensure Apache service is running and enabled
+service { 'apache2':
+  ensure => 'running',
+  enable => true,
+}
+
+# Ensure correct permissions for the web directory
+file { '/var/www/html':
+  ensure  => directory,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  recurse => true,
+}
+
+# Ensure Apache configuration files have correct permissions
+file { '/etc/apache2/apache2.conf':
+  ensure => file,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+}
+
+# Optionally: Adjust PHP configuration if necessary
+file { '/etc/php/7.4/apache2/php.ini':
+  ensure => file,
+  source => 'puppet:///modules/your_module/php.ini',
+  notify => Service['apache2'],
 }
