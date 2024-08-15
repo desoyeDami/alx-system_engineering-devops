83034a0077300a673ea0553dd4179d17d1e7f936
desoyeDami <desoyedami@gmail.com>
2024-08-15 01:14:05 +0100

    jsjs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,10 @@ def number_of_subscribers(subreddit):
     headers = {
         'User-Agent': 'My user agent 1.0'
     }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 1
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get('data', {})
+        sub_cnt = data.get('subscribers', 0)
+        return sub_cnt
+    else:
+        return 0
