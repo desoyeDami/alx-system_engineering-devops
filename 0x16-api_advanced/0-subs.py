63d1bea0b0603aa2ec23d60ef0dedd48e62014c1
desoyeDami <desoyedami@gmail.com>
2024-08-15 01:10:53 +0100

    jsjs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,11 @@ import requests
 def number_of_subscribers(subreddit):
     """ number of subscribers """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        'User-Agent': 'My user agent 1.0'
+    }
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
