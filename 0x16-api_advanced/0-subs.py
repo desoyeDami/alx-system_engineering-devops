13e3008d7d9fb3d9af525ee887ee3b320a36321b
desoyeDami <desoyedami@gmail.com>
2024-08-15 01:03:21 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-""" """
+""" Subreddit Subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ """
+    """ number of subscribers """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     try:
-        headers = {'User-agent': 'My user agent 1.0'}
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
         else:
             return 0
     except requests.RequestException:
-        return 0
+        return 1
