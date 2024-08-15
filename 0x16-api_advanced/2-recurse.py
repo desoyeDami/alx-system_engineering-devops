739ca92520ee4f96e0d9b6b73f0e75cb108f1de4
desoyeDami <desoyedami@gmail.com>
2024-08-15 21:36:08 +0100

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,21 +2,27 @@
 """ Querying Reddit """
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
-    """Recursively query a subreddit and return a list of titles of all hot posts."""
+    """Recursively query a subreddit and rtrn a lst of titles of hot posts."""
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'mybot/0.1'}
     params = {'after': after} if after else {}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+                            url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False
+                            )
 
     if response.status_code != 200:
         return None
 
     data = response.json().get("data", {})
     posts = data.get("children", [])
-    
+
     for post in posts:
         hot_list.append(post.get("data", {}).get("title"))
 
