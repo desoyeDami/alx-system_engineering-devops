1b9e5d6ea79aae2921630c391fb7fa83d6694e06
desoyeDami <desoyedami@gmail.com>
2024-08-18 14:33:55 +0100

    push

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # fix it and then automate it using Puppet
 exec { 'fix-wordpress':
     command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-    path    => '/usr/local/bin/:/bin/',
-    onlyif  => 'test -f /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
 }
